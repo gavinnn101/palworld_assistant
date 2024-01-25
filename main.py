6c9e4d37cb9dbb193dc9eccf4330930e852ef95a
gavinnn101 <41026935+gavinnn101@users.noreply.github.com>
2024-01-24 19:37:05 -0800

    initial commit with `auto f hold`

---
+++ main.py
@@ -0,0 +1,39 @@
+import pyautogui
+import keyboard
+import threading
+
+
+TOGGLE_KEY = "`"
+END_KEY = "esc"
+KEY_TO_HOLD = "f"
+
+toggle = False
+
+
+def on_triggered():
+    global toggle
+    toggle = not toggle
+
+    if toggle:
+        print(f"Holding {KEY_TO_HOLD} key...")
+        pyautogui.keyDown(KEY_TO_HOLD)
+    else:
+        print(f"Releasing {KEY_TO_HOLD} key...")
+        pyautogui.keyUp(KEY_TO_HOLD)
+
+
+def listen():
+    keyboard.add_hotkey(TOGGLE_KEY, on_triggered)
+
+    print(f"Listening for key press... Press {TOGGLE_KEY} to toggle holding {KEY_TO_HOLD} key.")
+    keyboard.wait(END_KEY)
+
+
+def main():
+    listen_thread = threading.Thread(target=listen)
+    listen_thread.start()
+    listen_thread.join()
+
+
+if __name__ == "__main__":
+    main()
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Palworld Assistant
+
+Easy afk crafting with this script that has a toggle to hold down a key for you.
+
+## Setup
+1. Install python if you don't already have it - https://www.python.org/downloads/
+2. In the folder where this file is located, press `shift + right-click` -> `open powershell window here`.
+3. Type and enter `pip install -r requirements.txt`.
+
+## Running the script
+1. Open `main.py` with notepad of your choice and edit `#GLOBALS` to what you want and then save the file.
+2. Run script by typing and entering `python main.py` into the same powershell window you used for setup.
